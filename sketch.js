819c1c06043f85883800cd012c274d5a6c034bbc
mgutie57 <mgutie57@uic.edu>
2022-01-19 11:07:48 -0600

    first commit message

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(800, 400);
+  }
+  
+  function draw() {
+    background(209, 201, 255);
+    strokeWeight(4);
+    stroke(0, 120, 32);
+    fill(255, 188, 94);
+    circle(400, 200, 40);
+    strokeWeight(2);
+    stroke(255, 247, 0);
+    fill(105, 51, 150);
+    circle(200, 100, 20);
+    nofill();
+    strokeWeight(1);
+    stroke(255, 255, 255);
+    circle(0, 0, 20);
+  }
